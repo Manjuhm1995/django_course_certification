c9f17ae819efa3f08495f0ddc47717a69d03f385
MANJU H M <manjuhm1995@gmailcom>
2023-07-13 19:09:24 +0530

    1st committing of special temlate feature

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from datetime import date
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
+        "image": "Manju H M.jpeg",
         "author":"Manju H M",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
@@ -20,10 +20,10 @@ posts = [
     },
     {
         "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
+        "image": "devaraayanadurgada betta.jpeg",
         "author":"Manju H M",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
+        "date": date(2021, 7, 22),
+        "title": "Devaraayana Durga",
         "content":"""
             Devarayanadurga or DD Hills which translates to “the fort of God”
             is a tranquil hill station located near Tumkur district in the state of Karnataka.
@@ -38,10 +38,10 @@ posts = [
 
     {
         "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
+        "image": "devaraayanadurgada betta.jpeg",
         "author":"Manju H M",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
+        "date": date(2021, 7, 19),
+        "title": "Nature at its best",
         "content":"""Devarayanadurga or DD Hills which translates to “the fort of God”
             is a tranquil hill station located near Tumkur district in the state of Karnataka.
             Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
@@ -51,10 +51,14 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
 # Create your views here.
 
 def starting_page(request):
-    return render(request,"blog/index.html")
+    sorted_posts=sorted(all_posts,key=get_date)
+    latests_posts=sorted_posts[-3:]
+    return render(request,"blog/index.html",{"posts":latests_posts})
 
 def posts(request):
     return render(request, "blog/all-posts.html")
