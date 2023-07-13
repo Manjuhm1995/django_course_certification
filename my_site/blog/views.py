8b5b4809db089bbc29ef71af2c3a39d4dea2066b
MANJU H M <manjuhm1995@gmailcom>
2023-07-13 13:24:23 +0530

    1st time committing the adding the 'dummy data ' to the views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,55 @@
 from django.shortcuts import render
+from datetime import date
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author":"Manju H M",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "content":"""
+            Devarayanadurga or DD Hills which translates to “the fort of God”
+            is a tranquil hill station located near Tumkur district in the state of Karnataka.
+            Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
+            to live among the trees and visit the beautifully crafted temples.
+        """,
+        "excerpt": """
+            There's nothing like the views you get when hiking in the mountains!
+            And I wasn't even prepared for what happened whilst was enjoying the view!
+        """
+    },
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author":"Manju H M",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "content":"""
+            Devarayanadurga or DD Hills which translates to “the fort of God”
+            is a tranquil hill station located near Tumkur district in the state of Karnataka.
+            Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
+            to live among the trees and visit the beautifully crafted temples.
+        """,
+        "excerpt": """
+            There's nothing like the views you get when hiking in the mountains!
+            And I wasn't even prepared for what happened whilst was enjoying the view!
+        """
+    },
+
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author":"Manju H M",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "content":"""Devarayanadurga or DD Hills which translates to “the fort of God”
+            is a tranquil hill station located near Tumkur district in the state of Karnataka.
+            Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
+            to live among the trees and visit the beautifully crafted temples.""",
+        "excerpt": """There's nothing like the views you get when hiking in the mountains!
+             And I wasn't even prepared for what happened whilst was enjoying the view!"""
+    }
+]
 
 # Create your views here.
 
