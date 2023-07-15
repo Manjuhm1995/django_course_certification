69be288e96436ebdd15d4fd8f428529961162c7d
MANJU H M <manjuhm1995@gmailcom>
2023-07-15 17:24:10 +0530

    1st commit of rendering the detail page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -6,3 +6,11 @@ from . models import Book
 def index(request):
     books=Book.objects.all()
     return render(request,"book_outlet/index.html",{"books":books})
+
+def book_detail(request,id):
+    books=Book.objects.get(id=id)
+    return render(request,"book_outlet/book_detail.html",{
+        "title":books.title,
+        "author":books.author,
+        "is_bestseller":books.is_bestselling,
+        "rating":books.rating})
