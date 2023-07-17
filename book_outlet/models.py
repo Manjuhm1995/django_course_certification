8bebd0cd8bfaffa2e66f7e2db4f515cc434f912b
MANJU H M <manjuhm1995@gmailcom>
2023-07-17 19:03:12 +0530

    1st time committing the adding slugfield and overidding save method

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 class Book(models.Model):
@@ -10,7 +11,11 @@ class Book(models.Model):
             validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True,max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug=models.SlugField(default="",null=False)# title> beauty of the earth, slug version=beauty-of-the-earth
 
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        return super().save(*args,**kwargs)
     def get_absolute_url(self):
         return reverse("book_detail",args=[self.id])
 
