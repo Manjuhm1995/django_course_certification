998e5b386e84bac05e4c212648b1804c449592a5
MANJU H M <manjuhm1995@gmailcom>
2023-07-17 12:27:38 +0530

    1st time committing the model urls

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404
 from . models import Book
 
 # Create your views here.
@@ -8,7 +9,11 @@ def index(request):
     return render(request,"book_outlet/index.html",{"books":books})
 
 def book_detail(request,id):
-    books=Book.objects.get(id=id)
+    # try:
+    #     books=Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+    books=get_object_or_404(Book,id=id)
     return render(request,"book_outlet/book_detail.html",{
         "title":books.title,
         "author":books.author,
