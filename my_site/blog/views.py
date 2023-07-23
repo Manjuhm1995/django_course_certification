b34d25bbd27d1c328e16c4db7962abab4dfe3b89
MANJU H M <manjuhm1995@gmailcom>
2023-07-23 07:49:15 +0530

    126_127_rendering_starting_allpost_single_pages committing for first time

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,70 +1,25 @@
-from django.shortcuts import render
-from datetime import date
-all_posts = [
-    {
-        "slug": "hike-in-the-mg",
-        "image": "Manju H M.jpeg",
-        "author":"Manju H M",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "content":"""
-            Devarayanadurga or DD Hills which translates to “the fort of God”
-            is a tranquil hill station located near Tumkur district in the state of Karnataka.
-            Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
-            to live among the trees and visit the beautifully crafted temples.
-        """,
-        "excerpt": """
-            There's nothing like the views you get when hiking in the mountains!
-            And I wasn't even prepared for what happened whilst was enjoying the view!
-        """
-    },
-    {
-        "slug": "hike-in-the-tk",
-        "image": "devaraayanadurgada betta.jpeg",
-        "author":"Manju H M",
-        "date": date(2021, 7, 22),
-        "title": "Devaraayana Durga",
-        "content":"""
-            Devarayanadurga or DD Hills which translates to “the fort of God”
-            is a tranquil hill station located near Tumkur district in the state of Karnataka.
-            Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
-            to live among the trees and visit the beautifully crafted temples.
-        """,
-        "excerpt": """
-            There's nothing like the views you get when hiking in the mountains!
-            And I wasn't even prepared for what happened whilst was enjoying the view!
-        """
-    },
+from django.shortcuts import render,get_object_or_404
+from .models import Post,Tag,Author
 
-    {
-        "slug": "hike-in-the-dd",
-        "image": "devaraayanadurgada betta.jpeg",
-        "author":"Manju H M",
-        "date": date(2021, 7, 19),
-        "title": "Nature at its best",
-        "content":"""Devarayanadurga or DD Hills which translates to “the fort of God”
-            is a tranquil hill station located near Tumkur district in the state of Karnataka.
-            Situated at an elevation of 4000 ft, Devarayanadurga is a perfect weekend getaway from Bangalore
-            to live among the trees and visit the beautifully crafted temples.""",
-        "excerpt": """There's nothing like the views you get when hiking in the mountains!
-             And I wasn't even prepared for what happened whilst was enjoying the view!"""
-    }
-]
-
-def get_date(post):
-    return post['date']
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts=sorted(all_posts,key=get_date)
-    latests_posts=sorted_posts[-3:]
+    latests_posts=Post.objects.all().order_by("-date")[:3]
+    # it just reduce the performance because just for 3 we are fething
+    # all posts and filtering and slicing in python
+
     return render(request,"blog/index.html",{"posts":latests_posts})
 
 def posts(request):
-    return render(request, "blog/all-posts.html",{"posts":all_posts})
+    latests_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html",{"posts":latests_posts})
 
 
 def post_detail(request,slug):
-    identified_post=next(post for post in all_posts if post["slug"]==slug)
+    # identified_post=Post.objects.get(slug=slug)
+    # above line may lead to the error so we need to use the try except block
+    # below line provide the same functionality without much code
+
+    identified_post=get_object_or_404(Post,slug=slug)
     return render(request, "blog/post-detail.html",{"posts":identified_post})
 
