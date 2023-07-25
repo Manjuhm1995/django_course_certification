b442a3228ccc02d127a5784beb471310a3113f76
MANJU H M <manjuhm1995@gmailcom>
2023-07-25 12:31:54 +0530

    1st time committing 137_manual_form_validation

--- reviews/views.py
+++ reviews/views.py
@@ -7,7 +7,7 @@ def review(request):
   if request.method == 'POST':
     entered_username = request.POST['username']
 
-    if entered_username == "" and len(entered_username) >= 100:
+    if entered_username == "":#and len(entered_username) >= 100
       return render(request, "reviews/review.html", {
         "has_error": True
       })
