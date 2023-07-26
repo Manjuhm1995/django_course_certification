1f7539f535520bacd767ffde757384e7771881bc
MANJU H M <manjuhm1995@gmailcom>
2023-07-26 17:40:34 +0530

    1st time committing of 147_saving_data_with_a_model_form

--- reviews/views.py
+++ reviews/views.py
@@ -7,14 +7,18 @@ from .models import Review
 
 def review(request):
   if request.method == 'POST':
+    #   in order to update the values in records, get  a object of the record
+    # existing_data= Review.objects.get(pk=1)
+    # form = ReviewForm(request.POST,instance=existing_data)
     form = ReviewForm(request.POST)
 
     if form.is_valid():
-      review=Review(user_name=form.cleaned_data["user_name"],
-                    review_text=form.cleaned_data["review_text"],
-                    rating=form.cleaned_data["rating"]
-                    )
-      review.save()
+      # review=Review(user_name=form.cleaned_data["user_name"],
+      #               review_text=form.cleaned_data["review_text"],
+      #               rating=form.cleaned_data["rating"]
+      #               )
+      # review.save()
+      form.save()
       return HttpResponseRedirect("/thank-you")
 
   else:
