32efa71c104d8c2076913b107f345f3880746592
MANJU H M <manjuhm1995@gmailcom>
2023-07-28 17:10:18 +0530

    1st time committing the 165_storing_the_uploaded_files

--- profiles/views.py
+++ profiles/views.py
@@ -11,12 +11,12 @@ class CreateProfileView(View):
         return render(request,"profiles/create_profile.html")
     def post(self,request):
         image_file=request.FILES["image"]
-        print(image_file)
-        # store_file(image_file)
+        # print(image_file)
+        store_file(image_file)
         return HttpResponseRedirect("/profiles")
-# def store_file(file):
-#     with open("tmp/image.jpg","wb+") as dest:
-#         for chunk in file.chunks():
-#             dest.write(chunk)
+def store_file(file):
+    with open("tmp/image.jpg","wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 
