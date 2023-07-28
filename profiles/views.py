09a6a99d49a969e9b04ff4400cb26abfe561e842
MANJU H M <manjuhm1995@gmailcom>
2023-07-28 17:58:34 +0530

    1st time committing the 166_adding_a_form_with_filefield

--- profiles/views.py
+++ profiles/views.py
@@ -4,19 +4,28 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views import View
 from django.http import HttpResponseRedirect
-# class CreateProfileView(TemplateView):
-#     template_name = "profiles/create_profile.html"
-class CreateProfileView(View):
-    def get(self,request):
-        return render(request,"profiles/create_profile.html")
-    def post(self,request):
-        image_file=request.FILES["image"]
-        # print(image_file)
-        store_file(image_file)
-        return HttpResponseRedirect("/profiles")
+from .forms import ProfileForm
+
+
 def store_file(file):
     with open("tmp/image.jpg","wb+") as dest:
         for chunk in file.chunks():
             dest.write(chunk)
+# class CreateProfileView(TemplateView):
+#     template_name = "profiles/create_profile.html"
+class CreateProfileView(View):
+    def get(self,request):
+        form=ProfileForm()
+        return render(request,"profiles/create_profile.html",{"form":form})
+    def post(self,request):
+        submitted_form=ProfileForm(request.POST,request.FILES)
+        if submitted_form.is_valid():
+            image_file=request.FILES["image"]
+            # print(image_file)
+            store_file(image_file)
+            return HttpResponseRedirect("/profiles")
+        return render(request,"profiles/create_profile.html",{"form":submitted_form})
+
+
 
 
