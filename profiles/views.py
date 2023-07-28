78ec4bf008345f55032ac86e505a88b2f7bf8903
MANJU H M <manjuhm1995@gmailcom>
2023-07-28 17:04:40 +0530

    1st time committing the 164_making_the_file_upload_work

--- profiles/views.py
+++ profiles/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import TemplateView
-class CreateProfileView(TemplateView):
-    template_name = "profiles/create_profile.html"
+from django.views import View
+from django.http import HttpResponseRedirect
+# class CreateProfileView(TemplateView):
+#     template_name = "profiles/create_profile.html"
+class CreateProfileView(View):
+    def get(self,request):
+        return render(request,"profiles/create_profile.html")
+    def post(self,request):
+        image_file=request.FILES["image"]
+        print(image_file)
+        # store_file(image_file)
+        return HttpResponseRedirect("/profiles")
+# def store_file(file):
+#     with open("tmp/image.jpg","wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+
+
