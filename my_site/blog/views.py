77f764dfa242a613ca787b67ebf877ffd662dd57
MANJU H M <manjuhm1995@gmailcom>
2023-08-03 20:47:32 +0530

    1st time committing the 194_outputting_comments

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -40,7 +40,8 @@ class SinglePostView(View):
         context={
             "posts":post,
             "posts_tags":post.tags.all(),
-            "comment_form":CommentForm()
+            "comment_form":CommentForm(),
+            "comments":post.comments.all().order_by("-id")
         }
 
         return render(request,"blog/post-detail.html",context)
@@ -55,7 +56,9 @@ class SinglePostView(View):
         context = {
             "posts": post,
             "posts_tags": post.tags.all(),
-            "comment_form": comment_form
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
+
         }
         return render(request,"blog/post-detail.html",context)
 
