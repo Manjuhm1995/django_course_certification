00a3f7937eee67b454e4a1f960367f9447e80ce0
MANJU H M <manjuhm1995@gmailcom>
2023-08-03 10:52:23 +0530

    1st time committing the 188_converting_views_into_class_based_views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Tag,Author
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
+class StartingPageView(ListView):
+    template_name ="blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+    def get_queryset(self):
+        query_set=super().get_queryset()
+        data=query_set[0:3]
+        return data
 
-def starting_page(request):
-    latests_posts=Post.objects.all().order_by("-date")[:3]
-    # it just reduce the performance because just for 3 we are fething
-    # all posts and filtering and slicing in python
-
-    return render(request,"blog/index.html",{"posts":latests_posts})
-
-def posts(request):
-    latests_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html",{"posts":latests_posts})
+class AllPostView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    ordering = ["-date"]
+    context_object_name = "posts"
 
 
-def post_detail(request,slug):
-    # identified_post=Post.objects.get(slug=slug)
-    # above line may lead to the error so we need to use the try except block
-    # below line provide the same functionality without much code
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts_tags"]=self.object.tags.all()
+        context["posts"]=self.object
 
-    identified_post=get_object_or_404(Post,slug=slug)
-    return render(request,
-                  "blog/post-detail.html",
-                  {"posts":identified_post,"posts_tags":identified_post.tags.all()}
-                  )
-
+        return context
